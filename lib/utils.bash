04854e6818fe15dc5e5545984a7228b8b2815586
Xie Yanbo <xieyanbo@gmail.com>
2025-07-11 11:31:25 +0800

    adapt todos

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for dua.
 GH_REPO="https://github.com/Byron/dua-cli"
 TOOL_NAME="dua"
 TOOL_TEST="dua --help"
@@ -31,8 +30,7 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if dua has other means of determining installable versions.
+	# simply list the tag names from GitHub releases.
 	list_github_tags
 }
 
@@ -88,7 +86,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert dua executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
